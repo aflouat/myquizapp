4cbb22f5433557058fef8c512baaf61196b5fbb4
Aflouat ABDEL WEDOUD <aflouat@gmail.com>
2025-01-11 16:50:34 +0100

    first commit
    init backend

---
+++ back/src/main/java/fr/tmsconsult/myquiz/MyquizApplication.java
@@ -0,0 +1,13 @@
+package fr.tmsconsult.myquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyquizApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/fr/tmsconsult/myquiz/MyquizApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tmsconsult.myquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
